6a2680276f3bacfe9234e4c940365856564f76d9
manavd821 <mitmanav6805@gmail.com>
2026-05-20 18:09:09 +0530

    fixing urls

--- frontend/src/components/VideoContainer/VideoContainer.tsx
+++ frontend/src/components/VideoContainer/VideoContainer.tsx
@@ -63,7 +63,7 @@ export default function App() {
     
             });
             roomRef.current = room;
-            const token_url = "https://webrtc-with-livekit.vercel.app/token?identity=Manav" + (Math.random() * 1000);
+            const token_url = "https://webrtc-with-livekit.onrender.com/token?identity=Manav" + Math.trunc(Math.random() * 1000);
     
             const res = await fetch(token_url);
             const {token , url} = await res.json();
